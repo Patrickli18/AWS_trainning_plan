98096e0f771912cd20b8b2d9dbea0f6913ec15b7
Patrickli18 <iittiljw@gmail.com>
2026-04-04 08:32:10 +0900

    update

---
+++ 00.Terraform/main.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  s3_use_path_style           = false
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    apigateway     = "http://192.168.1.130:4566"
+    apigatewayv2   = "http://192.168.1.130:4566"
+    cloudformation = "http://192.168.1.130:4566"
+    cloudwatch     = "http://192.168.1.130:4566"
+    dynamodb       = "http://192.168.1.130:4566"
+    ec2            = "http://192.168.1.130:4566"
+    es             = "http://192.168.1.130:4566"
+    elasticache    = "http://192.168.1.130:4566"
+    firehose       = "http://192.168.1.130:4566"
+    iam            = "http://192.168.1.130:4566"
+    kinesis        = "http://192.168.1.130:4566"
+    lambda         = "http://192.168.1.130:4566"
+    rds            = "http://192.168.1.130:4566"
+    redshift       = "http://192.168.1.130:4566"
+    route53        = "http://192.168.1.130:4566"
+    s3             = "http://s3.192.168.1.130.localstack.cloud:4566"
+    secretsmanager = "http://192.168.1.130:4566"
+    ses            = "http://192.168.1.130:4566"
+    sns            = "http://192.168.1.130:4566"
+    sqs            = "http://192.168.1.130:4566"
+    ssm            = "http://192.168.1.130:4566"
+    stepfunctions  = "http://192.168.1.130:4566"
+    sts            = "http://192.168.1.130:4566"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-20170727"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+#resource "aws_eip_association" "eip_assoc" {
+#  instance_id   = aws_instance.web.id
+#  allocation_id = aws_eip.example.id
+#}
+
+#resource "aws_eip" "example" {
+#  domain = "vpc"
+#}
+
+output "instance_id" {
+  value = aws_instance.web.id
+}
