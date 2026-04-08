993b8df4fd3864973622329bfa67d29e1f3f4cbe
Patrickli18 <iittiljw@gmail.com>
2026-04-08 10:19:09 +0900

    update

--- 00.Terraform/tes01.tf
+++ 00.Terraform/tes01.tf
@@ -43,31 +43,8 @@ provider "aws" {
   }
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-20170727"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
 output "instance_id" {
   value = aws_instance.web.id
