fa52f9a6f313dbee39cc7fec2331258af8a3fde9
Patrickli18 <iittiljw@gmail.com>
2026-05-06 16:54:56 +0900

    update

--- 00.Terraform/05_module-2/modules/database/main.tf
+++ 00.Terraform/05_module-2/modules/database/main.tf
@@ -8,9 +8,9 @@ resource "aws_db_instance" "database" {
   allocated_storage      = 10
   engine                 = "mysql"
   engine_version         = "8.0"
-  instance_class         = "db.t2.micro"
+  instance_class         = "db.t3.micro"
   identifier             = "${var.namespace}-db-instance"
-  name                   = "pets"
+  db_name                = "mydb"
   username               = "admin"
   password               = random_password.password.result
   db_subnet_group_name   = var.vpc.database_subnet_group #B
